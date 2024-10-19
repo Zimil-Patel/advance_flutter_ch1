abfb56b57eae39cf011db54f5ec08ba0703b2d80
Zimil-Patel <patelzimil2310@gmail.com>
2024-10-19 13:30:45 +0530

    19 Oct 24

--- android/app/src/main/kotlin/com/example/advance_flutter_ch1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/advance_flutter_ch1/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.advance_flutter_ch1
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import android.os.Bundle
 
-class MainActivity: FlutterFragmentActivity() {}
+class MainActivity: FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
